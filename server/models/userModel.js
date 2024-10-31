1b71ea3945664d09cae50a1a4b771a4708ba9491
Hazem Osama <hazem@debian.168.1.20>
2024-10-31 22:23:16 +0300

    Merging manual

---
+++ server/models/userModel.js
@@ -0,0 +1,63 @@
+import { clear } from 'console';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+
+
+const { Schema } = mongoose;
+const isEmail = validator.isEmail;
+
+//Schema for the User collection
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    immutable: true,
+    // Make sure the username does not contain special characters or spaces
+    validate: {
+      validator: function(v) {
+        return /^[a-z0-9_]*$/.test(v);
+      }, message: props => `${props.value} is not a valid username! Only letters, numbers, and underscores are allowed.`
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email'],
+  },
+  password: {type: String, required: true},
+  firstname: {type: String, required: false},
+  lastname: {type: String, required: false},
+  phone: {type: String, required: true, unique: true},
+  image: {type: String, required: false},
+  role: {type: String, default: "user", enum: ["user", "admin"] },
+},
+  {timestamps: true}
+);
+
+//Hashes the user password with bcrypt
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//Add a pre-update hook for admin premissions
+userSchema.pre('findOneAndUpdate', async function(next) {
+  const doc = await this.model.findOne(this.getQuery());
+  if (this._update.role && doc.role !== 'admin') {
+      return next(new Error('Only an admin can modify the name field.'));
+  }
+  next();
+});
+
+//Creates a model from User Schema
+const User = mongoose.model('User', userSchema);
+export default User;
